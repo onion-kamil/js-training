b579090727d79a5dd9c7a3b0a97b8557c90e44eb
Cebula Kamil <onion.kamil@gmail.com>
2019-09-22 11:10:08 +0200

    Fixed some bugs

--- arrows-keys-change-background/script.js
+++ arrows-keys-change-background/script.js
@@ -7,11 +7,12 @@ document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 const changeColor = (e) => {
     switch (e.keyCode) {
         case 38:
-            document.body.style.backgroundColor = `rgb(${red < 255 ?  red++ : red}, ${green < 255 ? red++ : red}, ${blue < 255 ? blue ++ : blue})`;
+            document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
             break;
 
         case 40:
-            document.body.style.backgroundColor = `rgb(${red > 0 ?  red-- : red}, ${green > 0 ? green-- : green}, ${blue > 0 ? blue-- : blue})`;
+            document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+            console.log(red, green, blue)
             break;
     }
 }
